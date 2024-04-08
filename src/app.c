848e3293561fc101b6146589efffb3509343cc48
Tom Huang <tomghuang@gmail.com>
2024-04-07 23:01:59 -0700

    feat: add minimum example for embedding tcl
    # [body]
    
    # [footer]
    
    # //////////////////////////////////////////////////////////////////////////////
    # [title.type]
    #  Must be one of the following:
    #  - fix: Commits that fix bugs
    #  - feat: Commits that add new features
    #  - refactor: Commits that change existing code without changing any behavior
    #  - perf: Special refactor commits that improve performance
    #  - style: Commits that do not affect meaning (white-space, formatting, etc.)
    #  - test: Commits that add tests or fixing existing tests
    #  - docs: Commits that affect documentation only
    #  - build: Commits that change build components (scripts, CI pipeline, etc.)
    #  - chore: Miscellaneous commits (modifying .gitignore, etc.)
    #  - tag: Commits for adding tags
    #
    # [title.scope]
    #  The scope should be the name of the npm package affected. The following is
    #  the list of supported scopes:
    #  - xa: XAgent (default)
    #  - usi: Unified Storage Interface
    #  - uls: Unified Log System
    #
    # [title.summary]
    #  Use the summary field to provide a succinct description of the change:
    #  - Use the imperative, present tense: "change" not "changed" nor "changes"
    #  - Don't capitalize the first letter
    #  - No period (.) at the end
    #
    # [body]
    #  Just as in the summary, use the imperative, present tense: "fix" not "fixed"
    #  nor "fixes".
    #
    #  Explain the motivation for the change in the commit message body. This commit
    #  message should explain why you are making the change. You can include a
    #  comparison of the previous behavior with the new behavior in order to
    #  illustrate the impact of the change.
    #
    # [footer]
    #  The footer can contain information about breaking changes and deprecations
    #  and is also the place to reference GitHub issues, JIRA tickets, and other PRs
    #  that this commit closes or is related to. For example:
    #
    #  Resolves XCAGENT-100
    #  Resolves XCAGENT-100, SEAL-1500, XCAGENT-512
    #
    #  BREAKING CHANGE: <breaking change summary>
    #  <BLANK LINE>
    #  <breaking change description + migration instructions>
    #  <BLANK LINE>
    #  <BLANK LINE>
    #  Resolves <JIRA ticket>
    #
    #  DEPRECATED: <what is deprecated>
    #  <BLANK LINE>
    #  <deprecation description + recommended update path>
    #  <BLANK LINE>
    #  <BLANK LINE>
    #  Resolves #<JIRA ticket>

---
+++ src/app.c
@@ -0,0 +1,25 @@
+#include <tcl.h>
+
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    Tcl_FindExecutable(argv[0]);
+    Tcl_Interp *interp = Tcl_CreateInterp();
+    if (interp == NULL) {
+        fprintf(stderr, "fail to create tcl interpreter\n");
+        return EXIT_FAILURE;
+    }
+
+    int rv = Tcl_Eval(interp, "puts \"hello world\"");
+    const char *result = Tcl_GetStringResult(interp);
+    if (rv != TCL_OK) {
+        fprintf(stderr, "fail to evaluate file: %s\n", result);
+        Tcl_DeleteInterp(interp);
+        return EXIT_FAILURE;
+    }
+
+    printf("%s", result);
+    Tcl_DeleteInterp(interp);
+    return EXIT_SUCCESS;
+}
